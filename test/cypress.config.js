7e93dac0244b9d77a5795633616976fbd1ddb355
Vinaud <leyenasd@gmail.com>
2022-08-11 20:56:52 -0300

    Update: Set default screen resolution for tests

--- test/cypress.config.js
+++ test/cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    viewportWidth: 1920,
+    viewportHeight: 1080
   },
 });
---
+++ test/cypress/e2e/login.cy.js
@@ -0,0 +1,5 @@
+describe('Login', () => {
+  it('deve logar com sucesso', () => {
+    cy.visit('http://localhost:3000')
+  })
+})
