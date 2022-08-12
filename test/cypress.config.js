43a49f8412c64e8d13e520bb6088a0ce0c2070d4
Vinaud <leyenasd@gmail.com>
2022-08-11 21:52:14 -0300

    Test: Check login scenarios

--- test/cypress.config.js
+++ test/cypress.config.js
@@ -1,12 +1,21 @@
 const { defineConfig } = require("cypress");
+const { cypressBrowserPermissionsPlugin } = require('cypress-browser-permissions')
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      config = cypressBrowserPermissionsPlugin(on, config)
+      return config
     },
     baseUrl: 'http://localhost:3000',
     viewportWidth: 1920,
-    viewportHeight: 1080
+    viewportHeight: 1080,
+    env:{
+      browserPermissions: {
+        notifications: "allow",
+        geolocation: "allow",
+      }
+    }
   },
 });
--- test/cypress/e2e/login.cy.js
+++ test/cypress/e2e/login.cy.js
@@ -1,5 +1,38 @@
 describe('Login', () => {
   it('deve logar com sucesso', () => {
-    cy.visit('/')
+
+    const user ={
+      instagram: '@vinaud',
+      password: 'pwd123',
+      name: 'Joao'
+    }
+    cy.login(user)
+
+    cy.loggedUser(user.name)
+    
+  })
+
+  it('não deve logar com senha incorreta', () =>{
+    const user ={
+      instagram: '@vinaud',
+      password: '123456',
+    }
+
+    cy.login(user)
+
+    cy.modalHaveText('Credenciais inválidas, tente novamente!')
+    
+  })
+
+  it('não deve logar com instagram inexistente', () =>{
+    const user ={
+      instagram: '@vvvvinaud',
+      password: 'pwd123',
+    }
+
+    cy.login(user)
+
+    cy.modalHaveText('Credenciais inválidas, tente novamente!')
   })
 })
+
