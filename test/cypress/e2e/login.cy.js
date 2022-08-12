9c7736828ffbede7e9043e19d2c4703729b1f9f4
Vinaud <leyenasd@gmail.com>
2022-08-12 15:29:27 -0300

    Test: Verify required fields at login

--- test/cypress/e2e/login.cy.js
+++ test/cypress/e2e/login.cy.js
@@ -1,3 +1,5 @@
+/// <reference types="Cypress" />
+
 describe('Login', () => {
   it('deve logar com sucesso', () => {
 
@@ -34,5 +36,53 @@ describe('Login', () => {
 
     cy.modalHaveText('Credenciais inválidas, tente novamente!')
   })
+
+  context('Validação dos campos obrigatórios', () =>{
+
+    it('não deve logar sem preencher o campo do instagram', () =>{
+
+      const user ={
+        instagram: '',
+        password: 'pwd123',
+      }
+
+      cy.visit('/')
+      cy.get('input[name=instagram]').clear()
+      cy.get('input[name=password]').type(user.password)
+      cy.contains('button', 'Entrar').click()
+
+      cy.modalHaveText('Por favor, informe o seu código do Instagram!')
+  
+    })
+    
+    it('não deve logar sem preencher o campo de senha', () =>{
+
+      const user ={
+        instagram: '@vinaud',
+        password: '',
+      }
+      
+      cy.visit('/')
+      cy.get('input[name=instagram]').type(user.instagram)
+      cy.get('input[name=password]').clear()
+      cy.contains('button', 'Entrar').click()
+
+      cy.modalHaveText('Por favor, informe a sua senha secreta!')
+      
+    })
+    
+    it('não deve logar sem preencher os campos necessários', () =>{
+
+      cy.visit('/')
+
+      cy.get('input[name=instagram]').clear()
+      cy.get('input[name=password]').clear()
+      cy.contains('button', 'Entrar').click()
+
+      cy.modalHaveText('Por favor, informe suas credenciais!')
+    })
+  })
 })
+
+
 
