1365b9586f657b5127ec21a94dda609593b3e57f
Vinaud <leyenasd@gmail.com>
2022-08-25 21:27:20 -0300

    Test: Register new foodtruck

--- test/cypress/e2e/create.cy.js
+++ test/cypress/e2e/create.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="Cypress" />
 import mapPage from '../support/pages/Map'
+import createPage from '../support/pages/Create'
 
 describe('Recomendação', () => {
     it('deve recomendar um foodtruck', () => {
@@ -12,22 +13,21 @@ describe('Recomendação', () => {
 
         const foodtruck = {
             name: 'Tienda del Chavo',
-            description: 'O melhor lugar pra tomar o suco de limão, que parece groselha, mas tem gosto de tamarindo',
+            details: 'O melhor lugar pra tomar o suco de limão, que parece groselha, mas tem gosto de tamarindo',
             opening_hours: 'das 14 às 20h',
             latitude: '-5.8112867',
-            longitude: '-35.2084129'
+            longitude: '-35.2084129',
+            open_on_weekends: false
         }
 
         cy.apiCreateUser(user)
         cy.uiLogin(user)
 
         mapPage.createLink()
-        cy.setGeolocation(foodtruck.latitude, foodtruck.longitude)
 
-        cy.wait(30000)
-
+        createPage.form(foodtruck)
+        createPage.submit()
        
-
-
+        createPage.modal.haveText('Food truck cadastrado com sucesso!')
     })
 });
--- test/cypress/support/pages/Create/index.js
+++ test/cypress/support/pages/Create/index.js
@@ -1,6 +1,29 @@
+import modal from '../Components/Modal'
+
 class CreatePage{
 
+    constructor() {
+        this.modal = modal
+    }
     
+    form(foodtruck){
+        cy.setGeolocation(foodtruck.latitude, foodtruck.longitude)
+        cy.get('input[name=name]').type(foodtruck.name)
+        cy.get('textarea[name=details]').type(foodtruck.details)
+        cy.get('input[name=opening-hours]').type(foodtruck.opening_hours)
+
+        cy.contains('button', foodtruck.open_on_weekends ? 'Sim' : 'Não').click()
+
+      //  if(foodtruck.open_on_weekends === 'Sim')
+      //      cy.contains('button', 'Sim').click()
+
+      //  if(foodtruck.open_on_weekends === 'Não')
+      //       cy.contains('button', 'Não').click()
+    }   
+
+    submit(){
+        cy.contains('button','Cadastrar').click()
+    }
 
 }
 
