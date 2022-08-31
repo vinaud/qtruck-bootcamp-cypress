21e04ed8638864d0ad7ac2476f5aa1438278c02a
Vinaud <leyenasd@gmail.com>
2022-08-31 20:03:10 -0300

    Test: Verify foodtruck recomendation alternative flux

--- test/cypress/e2e/create.cy.js
+++ test/cypress/e2e/create.cy.js
@@ -30,4 +30,53 @@ describe('Recomendação', () => {
        
         createPage.modal.haveText('Food truck cadastrado com sucesso!')
     })
+
+    it('não deve cadastrar foodtruck com o nome duplicado', () => {
+        const user = {
+            instagram: '@sanji',
+            password: 'pwd123',
+            name: 'Sanji'
+        }
+
+        const foodtruck = {
+            name: 'Baratie',
+            details: 'A melhor comida de todo o East Blue',
+            opening_hours: 'das 18 às 0h',
+            latitude: '-5.8112867',
+            longitude: '-35.2084129',
+            open_on_weekends: true
+        }
+
+        cy.apiCreateUser(user)
+        cy.apiLogin(user)
+        cy.apiCreateFoodTruck(foodtruck)
+        cy.uiLogin(user)
+        
+        mapPage.createLink()
+        createPage.form(foodtruck)
+        createPage.submit()
+        createPage.modal.haveText('Esse food truck já foi cadastrado!')
+    });
+
+    it('todos os campos são obrigatórios', () => {
+        
+        const user = {
+            instagram: '@heisenberg',
+            password: 'pwd123',
+            name: 'Heisenberg'
+        }
+
+        const foodtruck = {
+            latitude: '-5.8112867',
+            longitude: '-35.2084129',
+        }
+
+        cy.apiCreateUser(user)
+        cy.uiLogin(user)
+
+        mapPage.createLink()
+        cy.setGeolocation(foodtruck.latitude, foodtruck.longitude)
+        createPage.submit()
+        createPage.modal.haveText('Os campos nome, descrição e horário de funcionamento devem ser informados para recomendar um food truck!')
+    });
 });
--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -24,10 +24,25 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-
 import loginPage from './pages/Login'
 import mapPage from './pages/Map'
 
+Cypress.Commands.add('apiLogin', (user) => {
+
+    const payload = {
+        instagram: user.instagram,
+        password: user.password
+    }
+
+    cy.request({
+        url: 'http://localhost:3333/sessions',
+        method: 'POST',
+        body: payload
+    }).then(response => {
+        expect(response.status).to.eql(200)
+        Cypress.env('token', response.body.token)
+    })
+})
 
 Cypress.Commands.add('apiResetUser', (instagram) => {
     cy.request({
@@ -39,7 +54,7 @@ Cypress.Commands.add('apiResetUser', (instagram) => {
     })
 })
 
-Cypress.Commands.add('apiCreateUser', (payload => {
+Cypress.Commands.add('apiCreateUser', (payload) => {
 
     cy.apiResetUser(payload.instagram)
 
@@ -50,7 +65,20 @@ Cypress.Commands.add('apiCreateUser', (payload => {
     }).then(response => {
         expect(response.status).to.eql(201)
     })
-}))
+})
+
+Cypress.Commands.add('apiCreateFoodTruck', (payload) => {
+    cy.request({
+        url: 'http://localhost:3333/foodtrucks',
+        method: 'POST',
+        headers: {
+            'Authorization': Cypress.env('token')
+        },
+        body: payload
+    }).then(response => {
+        expect(response.status).to.eql(201)
+    })
+})
 
 Cypress.Commands.add('uiLogin', (user) => {
     loginPage.go()
